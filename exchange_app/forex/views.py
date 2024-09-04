03a9c6783c46df319e956336b96397c7df5ecae7
donfortune <donfortunet.df@gmail.com>
2024-09-04 11:36:21 +0100

    initial commit

---
+++ exchange_app/forex/views.py
@@ -0,0 +1,61 @@
+# forex/views.py
+from django.shortcuts import render
+
+def get_cards_info():
+    # Mock data for GreyFinance debit card exchange rate and supported services
+    cards_info = [
+        {
+            'card_name': 'GreyFinance',
+            'exchange_rate': '740 NGN/USD',
+            'supported_services': ['Udemy', 'Apple Music', 'Spotify', 'Netflix', 'Amazon Prime'],
+        },
+        {
+            'card_name': 'Barter by Flutterwave',
+            'exchange_rate': '745 NGN/USD',
+            'supported_services': ['Spotify', 'Google Play Store', 'Apple Music', 'Uber', 'Netflix'],
+        },
+        {
+            'card_name': 'ALAT by Wema',
+            'exchange_rate': '750 NGN/USD',
+            'supported_services': ['Amazon', 'eBay', 'Udemy', 'Google Ads', 'Apple Music'],
+        },
+        {
+            'card_name': 'UBA Dollar Card',
+            'exchange_rate': '760 NGN/USD',
+            'supported_services': ['PayPal', 'Amazon', 'Google Play Store', 'Netflix', 'Spotify'],
+        },
+        {
+            'card_name': 'GTBank Dollar Card',
+            'exchange_rate': '755 NGN/USD',
+            'supported_services': ['Apple Music', 'Spotify', 'Netflix', 'Amazon Prime', 'Uber'],
+        },
+        {
+            'card_name': 'Fairmoney Dollar Card',
+            'exchange_rate': '755 NGN/USD',
+            'supported_services': ['Apple Music', 'Spotify', 'Netflix', 'Amazon Prime', 'Uber'],
+        },
+        {
+            'card_name': 'Vella Dollar Card',
+            'exchange_rate': '755 NGN/USD',
+            'supported_services': ['Apple Music', 'Spotify', 'Netflix', 'Amazon Prime', 'Uber'],
+        },
+        {
+            'card_name': 'ChipperCash Dollar Card',
+            'exchange_rate': '755 NGN/USD',
+            'supported_services': ['Apple Music', 'Spotify', 'Netflix', 'Amazon Prime', 'Uber'],
+        },
+        {
+            'card_name': 'EcoBank Dollar Card',
+            'exchange_rate': '755 NGN/USD',
+            'supported_services': ['Apple Music', 'Spotify', 'Netflix', 'Amazon Prime', 'Uber'],
+        },
+    ]
+    return cards_info
+
+def home(request):
+    cards_info = get_cards_info()
+    context = {
+        'cards_info': cards_info
+    }
+    return render(request, 'home.html', context)
+
